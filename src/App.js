c6446d34b4a659c4e2de31fe85389bbf6970b04d
Evren Ceyhan <evren@ceyhan.io>
2022-04-26 23:04:05 +0300

    search movies api impl.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
+import { useEffect } from 'react';
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=90715111';
+
 const App = () => {
+    const searchMovies = async (title) => {
+        const url = `${API_URL}&s=${encodeURIComponent(title)}`;
+        const response = await fetch(url);
+        return await response.json();
+    };
+
+    useEffect(() => {
+        async function fetchData() {
+            const resp = await searchMovies('spiderman');
+            console.log(resp.Search);
+        }
+        fetchData();
+    }, []);
+
     return <h1>Hello World</h1>;
 };
 
